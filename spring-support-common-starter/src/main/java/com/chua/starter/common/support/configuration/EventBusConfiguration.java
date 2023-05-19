fc2c2ed64edc6fb06dc49084b921a16a04ddd63c
ACHTK <yemengzhidiandig@gmail.com>
2023-05-19 11:40:16 +0800

    调整代码

--- spring-support-common-starter/src/main/java/com/chua/starter/common/support/configuration/EventBusConfiguration.java
+++ spring-support-common-starter/src/main/java/com/chua/starter/common/support/configuration/EventBusConfiguration.java
@@ -1,44 +1,45 @@
-//package com.chua.starter.common.support.configuration;
-//
-//import com.chua.starter.core.support.eventbus.EventbusTemplate;
-//import org.springframework.beans.BeansException;
-//import org.springframework.beans.factory.config.SmartInstantiationAwareBeanPostProcessor;
-//import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-//import org.springframework.context.annotation.Bean;
-//
-//import java.util.concurrent.Executor;
-//
-///**
-// * @author CH
-// */
-//public class EventBusConfiguration  {
-//
-//
-//    @Bean
-//    @ConditionalOnMissingBean
-//    public EventbusTemplate EventbusTemplate(Executor executor) {
-//        return EventbusTemplate.create(executor);
-//    }
-//
-//    @AutoConfigureAfter({EventBusConfiguration.class, EventbusTemplate.class})
-//    public static class EventBussProcessor implements SmartInstantiationAwareBeanPostProcessor{
-//
-//        private EventbusTemplate EventbusTemplate;
-//        private static final String SPRING = "org.spring";
-//
-//        public EventBussProcessor(EventbusTemplate EventbusTemplate) {
-//            this.EventbusTemplate = EventbusTemplate;
-//        }
-//
-//        @Override
-//        public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
-//            String typeName = bean.getClass().getTypeName();
-//            if(!typeName.startsWith(SPRING)) {
-//                EventbusTemplate.register(bean);
-//            }
-//            return SmartInstantiationAwareBeanPostProcessor.super.postProcessAfterInstantiation(bean, beanName);
-//        }
-//
-//    }
-//}
+package com.chua.starter.common.support.configuration;
+
+import com.chua.starter.common.support.eventbus.EventbusTemplate;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.SmartInstantiationAwareBeanPostProcessor;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executor;
+
+/**
+ * @author CH
+ */
+public class EventBusConfiguration  {
+
+
+
+    @Bean
+    @ConditionalOnMissingBean
+    public EventbusTemplate EventbusTemplate(Executor executor) {
+        return (EventbusTemplate) EventbusTemplate.builder().executor(executor).build();
+    }
+
+    @AutoConfigureAfter({EventBusConfiguration.class, EventbusTemplate.class})
+    public static class EventBussProcessor implements SmartInstantiationAwareBeanPostProcessor{
+
+        private EventbusTemplate eventbusTemplate;
+        private static final String SPRING = "org.spring";
+
+        public EventBussProcessor(EventbusTemplate eventbusTemplate) {
+            this.eventbusTemplate = eventbusTemplate;
+        }
+
+        @Override
+        public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
+            String typeName = bean.getClass().getTypeName();
+            if(!typeName.startsWith(SPRING)) {
+                eventbusTemplate.register(bean);
+            }
+            return SmartInstantiationAwareBeanPostProcessor.super.postProcessAfterInstantiation(bean, beanName);
+        }
+
+    }
+}
---
+++ spring-support-common-starter/src/main/java/com/chua/starter/common/support/eventbus/EventbusTemplate.java
@@ -0,0 +1,10 @@
+package com.chua.starter.common.support.eventbus;
+
+import com.chua.common.support.eventbus.EventbusProvider;
+
+/**
+ * EventbusTemplate
+ * @author CH
+ */
+public class EventbusTemplate extends EventbusProvider {
+}
