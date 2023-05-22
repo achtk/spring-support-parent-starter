5205de01a760ba3892d0ec8a1cf49ce0bdec0124
ACHTK <yemengzhidiandig@gmail.com>
2023-05-22 17:26:05 +0800

    调整代码

---
+++ spring-support-starter-demo-client/src/main/java/com/chua/starter/client/ClientApplication.java
@@ -0,0 +1,15 @@
+package com.chua.starter.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author CH
+ */
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class);
+    }
+}
---
+++ spring-support-starter-demo-client/src/main/java/com/chua/starter/server/service/DemoService.java
@@ -0,0 +1,10 @@
+package com.chua.starter.server.service;
+
+/**
+ * @author CH
+ * @since 2022/6/10 15:53
+ */
+public interface DemoService {
+
+    String test();
+}
