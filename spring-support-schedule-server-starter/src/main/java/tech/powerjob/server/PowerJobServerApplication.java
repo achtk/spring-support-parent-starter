1845283ce2be2ddddbe4cd4cb813b3f5caac51f2
ACHTK <yemengzhidiandig@gmail.com>
2023-05-23 09:09:04 +0800

    调整代码

--- spring-support-schedule-server-starter/src/main/java/tech/powerjob/server/PowerJobServerApplication.java
+++ spring-support-schedule-server-starter/src/main/java/tech/powerjob/server/PowerJobServerApplication.java
@@ -1,10 +1,10 @@
 package tech.powerjob.server;
 
-import tech.powerjob.server.common.utils.PropertyUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import tech.powerjob.server.common.utils.PropertyUtils;
 
 /**
  * powerjob-server entry
@@ -18,8 +18,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class PowerJobServerApplication {
 
     public static void main(String[] args) {
+
         pre();
-        SpringApplication.run(PowerJobServerApplication.class, args);
+
+        // Start SpringBoot application.
+        try {
+            SpringApplication.run(PowerJobServerApplication.class, args);
+        } catch (Throwable t) {
+            throw t;
+        }
     }
 
     private static void pre() {
