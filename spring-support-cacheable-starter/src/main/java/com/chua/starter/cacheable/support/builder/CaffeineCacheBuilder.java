d28ed6e788d604da646a0af21ec6ae00d8acdd8b
ACHTK <yemengzhidiandig@gmail.com>
2023-06-10 00:06:38 +0800

    i18n

--- spring-support-cacheable-starter/src/main/java/com/chua/starter/cacheable/support/builder/CaffeineCacheBuilder.java
+++ spring-support-cacheable-starter/src/main/java/com/chua/starter/cacheable/support/builder/CaffeineCacheBuilder.java
@@ -2,8 +2,11 @@ package com.chua.starter.cacheable.support.builder;
 
 import com.chua.common.support.annotations.Spi;
 import com.chua.starter.cacheable.support.properties.CacheProperties;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.Cache;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * 缓存构建器
@@ -16,6 +19,8 @@ public class CaffeineCacheBuilder implements CacheBuilder {
     @Override
     public Cache build(String name, CacheProperties cacheProperties, org.springframework.boot.autoconfigure.cache.CacheProperties cacheProperties1) {
         CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setAllowNullValues(cacheManager.isAllowNullValues());
+        cacheManager.setCaffeine(Caffeine.newBuilder().expireAfterWrite(cacheProperties.getExpire(), TimeUnit.SECONDS));
         return cacheManager.getCache(name);
     }
 }
