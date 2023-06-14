a4e4bf6d587a93649835e9247758a0fe687a05b8
ACHTK <yemengzhidiandig@gmail.com>
2023-06-14 21:44:45 +0800

    i18n

--- spring-support-vuesql-starter/src/main/java/com/chua/starter/vuesql/support/channel/RedisTableChannel.java
+++ spring-support-vuesql-starter/src/main/java/com/chua/starter/vuesql/support/channel/RedisTableChannel.java
@@ -1,6 +1,8 @@
 package com.chua.starter.vuesql.support.channel;
 
 import com.chua.starter.vuesql.entity.system.WebsqlConfig;
+import com.chua.starter.vuesql.enums.Action;
+import com.chua.starter.vuesql.enums.Type;
 import com.chua.starter.vuesql.pojo.Construct;
 import com.chua.starter.vuesql.pojo.Keyword;
 import com.chua.starter.vuesql.pojo.SqlResult;
@@ -9,8 +11,12 @@ import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.redisson.config.SingleServerConfig;
 import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * redis
@@ -26,7 +32,12 @@ public class RedisTableChannel implements TableChannel {
 
     @Override
     public List<Construct> getDataBaseConstruct(WebsqlConfig config) {
-        return null;
+        List<Construct> rs = new LinkedList<>();
+        rs.add(Construct.builder().type(Type.DATABASE).id(1).pid(0).name(config.getConfigDatabase()).build());
+        rs.addAll(IntStream.range(2, 18).mapToObj(it -> {
+            return Construct.builder().id(it).pid(1).type(Type.TABLE).action(Action.OPEN).name(it + "").build();
+        }).collect(Collectors.toList()));
+        return rs;
     }
 
     @Override
@@ -49,5 +60,10 @@ public class RedisTableChannel implements TableChannel {
         SingleServerConfig singleServerConfig = config.useSingleServer();
         singleServerConfig.setAddress("redis://" + websqlConfig.getConfigIp() + ":" + websqlConfig.getConfigPort()).setPassword(websqlConfig.getConfigPassword());
         return Redisson.create(config);
+    }
+
+    public Jedis jedis(WebsqlConfig config) {
+        Jedis jedis = new Jedis(config.getConfigIp(), config.getConfigPort());
+        return jedis;
     }
 }
