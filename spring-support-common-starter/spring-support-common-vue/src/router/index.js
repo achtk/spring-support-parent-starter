0efb1bae5f34d50e46113006990f280b2b8a337c
ACHTK <yemengzhidiandig@gmail.com>
2023-06-23 22:29:21 +0800

    i18n

--- spring-support-common-starter/spring-support-common-vue/src/router/index.js
+++ spring-support-common-starter/spring-support-common-vue/src/router/index.js
@@ -3,14 +3,22 @@ import Home from "@/view/home.vue"
 
 
 const routes = [{
-        path: "/home",
-        name: 'home',
-        component: Home
-    }, {
-        path: "/oss",
-        name:"oss",
-        component: () => import('@/view/oss.vue')
-    }];
+    path: "/home",
+    name: 'home',
+    component: Home
+}, {
+    path: "/oss",
+    name: "oss",
+    component: () => import('@/view/oss.vue')
+}, {
+    path: "/test",
+    name: "test",
+    component: () => import('@/view/test.vue')
+}, {
+    path: "/test2",
+    name: "test2",
+    component: () => import('@/page/base/base.vue')
+}];
 
 
 const router = createRouter({
---
+++ spring-support-common-starter/spring-support-common-vue/src/view/test2.vue
@@ -0,0 +1,22 @@
+<template>
+  <butterfly
+      ref="butterfly"
+      :canvasData="graphData"
+      :canvasConf="canvasConfig"
+      @onLoaded="onLoaded"
+  >
+  </butterfly>
+</template>
+
+<script>
+import butterfly from "@/components/butterfly.vue";
+
+export default {
+  name: "test2",
+  components: {butterfly}
+}
+</script>
+
+<style scoped>
+
+</style>
