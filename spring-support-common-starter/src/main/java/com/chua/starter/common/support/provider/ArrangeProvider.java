fcf7881eee53127b8f7543127be6faaa214e67db
ACHTK <yemengzhidiandig@gmail.com>
2023-06-26 20:27:28 +0800

    no commit message

--- spring-support-common-starter/src/main/java/com/chua/starter/common/support/provider/ArrangeProvider.java
+++ spring-support-common-starter/src/main/java/com/chua/starter/common/support/provider/ArrangeProvider.java
@@ -1,7 +1,8 @@
 package com.chua.starter.common.support.provider;
 
+import com.alibaba.fastjson2.JSONObject;
+import com.chua.common.support.bean.BeanUtils;
 import com.chua.common.support.database.AutoMetadata;
-import com.chua.common.support.database.orm.conditions.Wrapper;
 import com.chua.common.support.database.orm.conditions.Wrappers;
 import com.chua.common.support.database.repository.Repository;
 import com.chua.common.support.lang.arrange.ArrangeHandler;
@@ -10,14 +11,16 @@ import com.chua.common.support.spi.Option;
 import com.chua.common.support.spi.ServiceProvider;
 import com.chua.starter.common.support.annotations.EnableAutoTable;
 import com.chua.starter.common.support.pojo.SysArrange;
+import com.chua.starter.common.support.pojo.SysArrangeEdge;
+import com.chua.starter.common.support.pojo.SysArrangeNode;
 import com.chua.starter.common.support.result.Result;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.*;
 
 import javax.sql.DataSource;
+import javax.transaction.Transactional;
 import java.util.List;
 
 /**
@@ -26,10 +29,13 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("${plugin.arrange.context-path:/arrange}")
-@EnableAutoTable(packageType = SysArrange.class)
+@EnableAutoTable(packageType = {SysArrange.class, SysArrangeEdge.class, SysArrangeNode.class})
 public class ArrangeProvider implements ApplicationContextAware {
 
     private Repository<SysArrange> repository;
+    private Repository<SysArrangeNode> nodeRepository;
+    private Repository<SysArrangeEdge> edgeRepository;
+
     /**
      * 所有任务
      * @return 所有任务
@@ -47,7 +53,28 @@ public class ArrangeProvider implements ApplicationContextAware {
     public Result<Page<SysArrange>> page(Page<SysArrange> page) {
         return Result.success(repository.page(page));
     }
+    /**
+     * 保存/更新
+     * @param json 保存
+     * @return 结果
+     */
+    @PostMapping("/saveOrUpdateNode")
+    @Transactional
+    public Result<Integer> saveOrUpdateNode(@RequestBody JSONObject json) {
+        String arrangeId = json.getString("arrangeId");
+        if(!repository.exist(Wrappers.<SysArrange>lambdaQuery().eq(SysArrange::getArrangeId, arrangeId))) {
+            return Result.failed("任务名称不存在");
+        }
+        List<SysArrangeNode> nodes = BeanUtils.copyPropertiesList(json.getJSONArray("nodes"), SysArrangeNode.class);
+        nodeRepository.delete(Wrappers.<SysArrangeNode>lambdaQuery().eq(SysArrangeNode::getArrangeId, arrangeId));
+        nodeRepository.saveBatch(nodes);
 
+        List<SysArrangeEdge> edges = BeanUtils.copyPropertiesList(json.getJSONArray("edges"), SysArrangeEdge.class);
+        edgeRepository.delete(Wrappers.<SysArrangeEdge>lambdaQuery().eq(SysArrangeEdge::getArrangeId, arrangeId));
+        edgeRepository.saveBatch(edges);
+
+        return Result.success();
+    }
     /**
      * 保存/更新
      * @param sysArrange 保存
@@ -77,5 +104,7 @@ public class ArrangeProvider implements ApplicationContextAware {
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         AutoMetadata autoMetadata = AutoMetadata.builder().build();
         this.repository = autoMetadata.createRepository(applicationContext.getBeansOfType(DataSource.class), SysArrange.class);
+        this.nodeRepository = autoMetadata.createRepository(applicationContext.getBeansOfType(DataSource.class), SysArrangeNode.class);
+        this.edgeRepository = autoMetadata.createRepository(applicationContext.getBeansOfType(DataSource.class), SysArrangeEdge.class);
     }
 }
