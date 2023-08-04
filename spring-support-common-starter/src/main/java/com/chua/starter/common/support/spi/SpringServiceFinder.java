8c143e3c20612b67d4bf749708d0f6af630fe18b
ACHTK <yemengzhidiandig@gmail.com>
2023-08-04 16:19:16 +0800

    1、新增图像库映射

---
+++ spring-support-common-starter/src/main/java/com/chua/starter/common/support/spi/SpringServiceFinder.java
@@ -0,0 +1,26 @@
+package com.chua.starter.common.support.spi;
+
+import com.chua.common.support.spi.ServiceDefinition;
+import com.chua.common.support.spi.finder.AbstractServiceFinder;
+import com.chua.starter.common.support.configuration.SpringBeanUtils;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * spring
+ * @author CH
+ */
+public class SpringServiceFinder extends AbstractServiceFinder {
+
+    @Override
+    protected List<ServiceDefinition> find() {
+        List<ServiceDefinition> rs = new LinkedList<>();
+        Map<String, ?> beansOfType = SpringBeanUtils.getApplicationContext().getBeansOfType(service);
+        for (Object o : beansOfType.values()) {
+            rs.addAll(buildDefinition(o));
+        }
+        return rs;
+    }
+}
