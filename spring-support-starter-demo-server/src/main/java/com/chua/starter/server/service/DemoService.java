cab6747f750812f924e06c58d40e6f7cf9d93437
ACHTK <yemengzhidiandig@gmail.com>
2023-09-09 11:59:38 +0800

    no commit message

--- spring-support-starter-demo-server/src/main/java/com/chua/starter/server/service/DemoService.java
+++
@@ -1,9 +0,0 @@
-package com.chua.starter.server.service;
-
-/**
- * @author CH
- */
-public interface DemoService {
-
-    String test();
-}
--- spring-support-websql-starter/src/main/java/org/springframework/base/system/utils/Constants.java
+++
@@ -1,7 +0,0 @@
-package org.springframework.base.system.utils;
-
-public final class Constants
-{
-    public static String DATABASETYPE3 = "MySql";
-    
-}
