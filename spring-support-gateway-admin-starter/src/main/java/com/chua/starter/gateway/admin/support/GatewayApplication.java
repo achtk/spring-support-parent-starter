2ce4dfd0e149453409f1b079fd41b4f2b3b65d23
ACHTK <yemengzhidiandig@gmail.com>
2023-09-10 21:02:04 +0800

    fix 1.统一配置中心添加监控管理

---
+++ spring-support-gateway-admin-starter/src/main/java/com/chua/starter/gateway/admin/support/GatewayApplication.java
@@ -0,0 +1,23 @@
+package com.chua.starter.gateway.admin.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.ldap.LdapAutoConfiguration;
+
+/**
+ * 网关应用程序
+ *
+ * @author CH
+ * @since 2023/09/10
+ */
+@SpringBootApplication(
+        exclude = {LdapAutoConfiguration.class},
+        scanBasePackages = "org.apache.shenyu.admin"
+)
+public class GatewayApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
